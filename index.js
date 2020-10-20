99628e1cbe6d6e7f8117ff7af57c6727854f9aa8
YeremiaIsrael <72957579+YeremiaIsrael@users.noreply.github.com>
2020-10-21 02:05:42 +0800

    update index.js

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ window.addEventListener('scroll',function(){
     nav.classList.toggle('posisi');
   }
 })
+const tbody=document.querySelector('tbody');
+const form =document.querySelector('form');
+const studentinfo = {
+  id:document.querySelector('#id'),
+  name:document.querySelector('#name')
+}
+form.addEventListener('submit',forminput);
+
+document.querySelector('.searchbar').addEventListener('keyup',function(){
+  var nilaisearch = this.value.toUpperCase();
+  var data =tbody.getElementsByTagName('tr')
+  for (var i = 0; i < data.length; i++) {
+    const nilai = data[i].getElementsByTagName('td')[1];
+    if (nilai.textContent.toUpperCase().includes(nilaisearch)>0) {
+      data[i].style.display = ''
+    }else{
+      data[i].style.display = 'none'
+    }
+  }
+})
+
+function forminput(e){
+  e.preventDefault();
+
+}
